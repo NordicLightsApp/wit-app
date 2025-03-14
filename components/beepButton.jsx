411552f84f8964fecba13b887f12ede6be69adb5
Lizene <olpertti@gmail.com>
2025-03-14 20:16:51 +0200

    react-native-sound-player

--- components/beepButton.jsx
+++ components/beepButton.jsx
@@ -1,13 +1,27 @@
 import { useState, useEffect } from "react"
 import { Text, Button, View, Vibration } from "react-native"
-import { SoundPlayer } from "react-native-sound-player"
+import Sound from "react-native-sound"
+// Enable playback in silence mode
+Sound.setCategory('Playback');
 
 export const BeepButtonComponent = () => {
     const [buttonState, SetButtonState] = useState(false)
     const [text, SetText] = useState("Click to beep")
+    //SoundPlayer.loadSoundFile("beep","wav");
+    var beep = new Sound('beep.wav', Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          console.log('failed to load beep-sound', error);
+          return;
+        }});
     const playBeep = () => {
-        SoundPlayer.playAsset(require("./beep.wav"))
-    }
+        beep.play()
+        /*
+        try {
+            SoundPlayer.play();
+        } catch (e){
+            console.log(`cannot play beep-sound file`, e);
+        }*/
+    };
 
     /*useEffect(() => {
 
