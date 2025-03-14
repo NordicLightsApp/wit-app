17df18a57bcfc9df9508080c78623f84f81684b7
Lizene <olpertti@gmail.com>
2025-03-14 12:21:35 +0200

    audiooo im tryyiinnnggg

--- components/beepButton.jsx
+++ components/beepButton.jsx
@@ -1,15 +1,23 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Text, Button, View, Vibration } from "react-native"
+import { SoundPlayer } from "react-native-sound-player"
 
 export const BeepButtonComponent = () => {
     const [buttonState, SetButtonState] = useState(false)
     const [text, SetText] = useState("Click to beep")
+    const playBeep = () => {
+        SoundPlayer.playAsset(require("./beep.wav"))
+    }
+
+    /*useEffect(() => {
+
+    },[buttonState])*/
     const handleClick = () => {
         SetText(buttonState ? "BEEP BEEP BEEP" : "Click to beep")
         SetButtonState(!buttonState)
         Vibration.vibrate([300, 200, 300, 200, 300, 200]);
     }
     return <View>
-        <Button onPress = {handleClick} title = {text} />
+        <Button onPress = {playBeep} title = {text} />
     </View>
 }
