9a01df2df9416ff1b39500e2a18802835e730e54
Lizene <olpertti@gmail.com>
2025-03-17 20:13:41 +0200

    interval setup

--- components/beepButton.jsx
+++ components/beepButton.jsx
@@ -1,28 +1,43 @@
 import { useState, useEffect } from "react"
 import { Text, Button, View, Vibration } from "react-native"
 import {useAudioPlayer} from "expo-audio"
-const beepSource = require("./BeepKeyboard/beepMiddleE.wav")
+const beepSources = [
+    require("./BeepKeyboardEMajor/beepMiddleE.wav"),
+    require("./BeepKeyboardEMajor/beepHighA.wav")]
 
 export const BeepButtonComponent = () => {
-    const [buttonState, SetButtonState] = useState(false)
-    const [text, SetText] = useState("Click to beep")
-    const beepPlayer = useAudioPlayer(beepSource)
-    
-
-    const playBeep = () => {
-        beepPlayer.loop = true
+    const [calibrating, SetCalibrating] = useState(false)
+    const [calibrationStartedOnce, SetCalibrationStartedOnce] = useState(false)
+    const [text, SetText] = useState("Start angle setup")
+    useEffect(()=>{
+        if(!calibrationStartedOnce) return;
+        var currentAngleSoundInterval
+        var targetAngleSoundInterval;
+        if (calibrating){
+            console.log("deedee")
+            
+            currentAngleSoundInterval = setInterval(playCurrentBeep, 1000)
+            //setTimeout(() => {targetAngleSoundInterval = setInterval(playTargetBeep), 1000}, 200)
+        } else{
+            console.log("megadoodoo")
+            
+        }
+    },[calibrating])
+    function playCurrentBeep(){
+        const beepPlayer = useAudioPlayer(beepSources[1])
         beepPlayer.play()
-    };
-
-    /*useEffect(() => {
-
-    },[buttonState])*/
+    }
+    function playTargetBeep(){
+        const beepPlayer = useAudioPlayer(beepSources[0])
+        beepPlayer.play()
+    }
     const handleClick = () => {
-        SetText(buttonState ? "BEEP BEEP BEEP" : "Click to beep")
-        SetButtonState(!buttonState)
-        Vibration.vibrate([300, 200, 300, 200, 300, 200]);
+        SetText((calibrating ? "Start" : "Stop") + " angle setup")
+        SetCalibrating(!calibrating)
+        SetCalibrationStartedOnce(true)
+        Vibration.vibrate([0, 100, 100, 100, 100, 100]);
     }
     return <View>
-        <Button onPress = {playBeep} title = {text} />
+        <Button onPress = {handleClick} title = {text} />
     </View>
 }
