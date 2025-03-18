8a6f7f921f394cda817413c6f43ee4747f2c74a5
JasminF <60253960+40412@users.noreply.github.com>
2025-03-18 16:49:22 +0200

    add Stop Search button

--- components/angles.jsx
+++ components/angles.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import {
+  Appearance,
   Button,
   NativeModules,
   Pressable,
   StyleSheet,
   Text,
-  Appearance,
-  useColorScheme,
   View,
 } from "react-native";
+import { SetGoal } from "./setGoalAngle";
 
 export const AngleData = () => {
   const { BluetoothModule } = NativeModules;
@@ -24,33 +24,33 @@ export const AngleData = () => {
   const [valueZ, setValueZ] = useState(0);
   const [searching, setSearching] = useState(false);
   var colors = {
-  	"background": "#FFF",
-  	"foreground": "#000",
-  	"primary": "#FF8200",
-  	"gray": "#D9D9D6",
-  	"secondary": "#32C8BE",
-  	"complementary": "#A9D52D"
+    background: "#FFF",
+    foreground: "#000",
+    primary: "#FF8200",
+    gray: "#D9D9D6",
+    secondary: "#32C8BE",
+    complementary: "#A9D52D",
   };
 
   const changeTheme = (preferences) => {
-  console.log(preferences.colorScheme);
-  console.log(typeof(preferences.colorScheme));
-  	switch (preferences.colorScheme) {
-  		case 'dark':
-  			colors.background = "#000";
-  			colors.foreground = "#FFF";
-  		break;
-  		case 'light':
-  		default:
-  			colors.background = "#FFF";
-  			colors.foreground = "#000";
-  		break;
-  	}
+    /* console.log(preferences.colorScheme);
+    console.log(typeof preferences.colorScheme); */
+    switch (preferences.colorScheme) {
+      case "dark":
+        colors.background = "#000";
+        colors.foreground = "#FFF";
+        break;
+      case "light":
+      default:
+        colors.background = "#FFF";
+        colors.foreground = "#000";
+        break;
+    }
   };
-    //switch between light and dark mode
+  //switch between light and dark mode
   Appearance.addChangeListener(changeTheme);
 
-	changeTheme({"colorScheme": Appearance.getColorScheme()});
+  changeTheme({ colorScheme: Appearance.getColorScheme() });
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
@@ -74,6 +74,11 @@ export const AngleData = () => {
   const startSearch = () => {
     setSearching(true);
     BluetoothModule.startDiscoveryR();
+  };
+
+  const stopSearch = () => {
+    setSearching(false);
+    BluetoothModule.stopDiscoveryR();
   };
 
   const refreshData = async () => {
@@ -107,35 +112,51 @@ export const AngleData = () => {
 
   const updateAngles = () => {
     if (angleX && angleY && angleZ) {
-      let x = Number(angleX.replace(",", "."));
+      let x = +angleX.replace(",", ".");
       let calculatedX = x - valueX;
       setZeroedX(calculatedX.toFixed(2));
-      let y = Number(angleY.replace(",", "."));
+      let y = +angleY.replace(",", ".");
       let calculatedY = y - valueY;
       setZeroedY(calculatedY.toFixed(2));
-      let z = Number(angleZ.replace(",", "."));
+      let z = +angleZ.replace(",", ".");
       let calculatedZ = z - valueZ;
       setZeroedZ(calculatedZ.toFixed(2));
     }
   };
 
   return (
-    <View style={[styles.container,{backgroundColor: colors.background}]}>
-      <Text style={{color: colors.foreground}}>Click to start discovering devices</Text>
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <Text style={{ color: colors.foreground }}>
+        Click to start discovering devices
+      </Text>
       <Button
-      	color={colors.primary}
+        color={colors.primary}
         disabled={deviceName !== "" || searching}
         title="Start search"
         onPress={startSearch}
       />
-      {searching && <Text style={{color: colors.foreground}}>Searching...</Text>}
-      <Text style={{color: colors.foreground}}>{deviceName}</Text>
-      <Text style={{color: colors.foreground}}>Angle X: {angleX}</Text>
-      <Text style={{color: colors.foreground}}>Angle Y: {angleY}</Text>
-      <Text style={{color: colors.foreground}}>Angle Z: {angleZ}</Text>
-      <Text style={[styles.angleText,{color: colors.foreground}]}>Zeroed X: {zeroedX} </Text>
-      <Text style={[styles.angleText,{color: colors.foreground}]}>Zeroed Y: {zeroedY} </Text>
-      <Text style={[styles.angleText,{color: colors.foreground}]}>Zeroed Z: {zeroedZ} </Text>
+      <Button
+        title="Stop Search"
+        color={colors.primary}
+        disabled={!searching}
+        onPress={stopSearch}
+      />
+      {searching && (
+        <Text style={{ color: colors.foreground }}>Searching...</Text>
+      )}
+      <Text style={{ color: colors.foreground }}>{deviceName}</Text>
+      <Text style={{ color: colors.foreground }}>Angle X: {angleX}</Text>
+      <Text style={{ color: colors.foreground }}>Angle Y: {angleY}</Text>
+      <Text style={{ color: colors.foreground }}>Angle Z: {angleZ}</Text>
+      <Text style={[styles.angleText, { color: colors.foreground }]}>
+        Zeroed X: {zeroedX}{" "}
+      </Text>
+      <Text style={[styles.angleText, { color: colors.foreground }]}>
+        Zeroed Y: {zeroedY}{" "}
+      </Text>
+      <Text style={[styles.angleText, { color: colors.foreground }]}>
+        Zeroed Z: {zeroedZ}{" "}
+      </Text>
       <Pressable
         style={{
           backgroundColor: colors.primary,
@@ -146,8 +167,9 @@ export const AngleData = () => {
       >
         <Text>Zero angles</Text>
       </Pressable>
+      <SetGoal x={zeroedX} z={zeroedZ} />
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
